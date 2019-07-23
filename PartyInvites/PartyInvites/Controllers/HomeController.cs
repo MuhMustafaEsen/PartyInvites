bc0c6846b1611dbb2db9144e89e5d63c4fecdd93
mustafa esen <esenmustafa42@gmail.com>
2019-07-23 19:21:15 +0300

    MVC Core PartyInvites

---
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.hour = hour < 12 ? "Good morning " : "Good Afternoon";
+            return View("MyView");
+        }
+        [HttpGet]
+        public ViewResult RsvpForm() {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.addResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                //there is a validation error burası valitadion hatası olunca.  
+                return View();
+            }
+            
+        }
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(x=>x.WillAttend == true));
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PartyInvites/PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage ="Please enter your name (Lütfen adınızı giriniz)")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Please enter your email (Lütfen mail adresi giriniz)")]
+        [RegularExpression(".+\\@.+\\..+")]//regular expression ekledik
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your Phone number (Lütfen telefon no giriniz)")]
+        public string  Phone { get; set; }
+        [Required(ErrorMessage = "Please specify whether you will attend (Lütfen Katilip katilmayacagınızı belirtiniz)")]
+        public bool? WillAttend { get; set; }
+
+    }
+
+}
